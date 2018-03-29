b9dab6f1f44a774331a202366a0d0211b01b0f59
Charlotte Cady <charlottecady@Unknown.gateway>
2018-03-30 10:45:09 +1100

    add champion route

--- client/src/components/ChampionsStats/ChampionStats.js
+++ client/src/components/ChampionsStats/ChampionStats.js
@@ -1,43 +1,46 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 // local import
 import Destiny from '../../assets/images/champions/Destiny.jpg';
 
 const ChampionStats = () => {
   return (
-    <div className="card-details">
-      <div className="card-image">
-        <img src={Destiny} alt="Destiny"/>
-        <p>Destiny</p>
-      </div>
-      <div className="card-data">
-        <div className="win-rate">
-          <p className="data">52 %</p>
-          <progress value="22" max="100">
-          </progress>
-          <p className="label">Win rate</p>
+    <Link to="/champions-stats/destiny">
+      <div className="card-details">
+        <div className="card-image">
+          <img src={Destiny} alt="Destiny"/>
+          <p>Destiny</p>
         </div>
-        <div className="pick-rate">
-          <p className="data">58 %</p>
-          <progress value="22" max="100">
-          </progress>
-          <p className="label">Pick rate</p>
+        <div className="card-data">
+          <div className="win-rate">
+            <p className="data">52 %</p>
+            <progress value="22" max="100">
+            </progress>
+            <p className="label">Win rate</p>
+          </div>
+          <div className="pick-rate">
+            <p className="data">58 %</p>
+            <progress value="22" max="100">
+            </progress>
+            <p className="label">Pick rate</p>
+          </div>
+          <ul className="average-stats">
+            <li>Average damage
+              <span className="data">250</span>
+            </li>
+            <li>Average healing
+              <span className="data">150</span>
+            </li>
+            <li>Average disable
+              <span className="data">400</span>
+            </li>
+            <li>Average score
+              <span className="data">800</span>
+            </li>
+          </ul>
         </div>
-        <ul className="average-stats">
-          <li>Average damage
-            <span className="data">250</span>
-          </li>
-          <li>Average healing
-            <span className="data">150</span>
-          </li>
-          <li>Average disable
-            <span className="data">400</span>
-          </li>
-          <li>Average score
-            <span className="data">800</span>
-          </li>
-        </ul>
       </div>
-    </div>
+    </Link>
   );
 }
 
