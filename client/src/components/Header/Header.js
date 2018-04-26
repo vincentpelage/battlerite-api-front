0d7b4b7ac81c7d396e369a456172085c0de7df03
Charlotte Cady <charlottecady@MacBook-Pro-de-Charlotte.local>
2018-04-26 12:29:50 +1000

    modification css suite aux retours utilisateurs

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React, { Fragment, Component } from 'react';
+import { Link } from "react-router-dom";
+
 // local import
 import getPathname from './utils/getPathname';
 import isNotChampionUrl from './utils/isNotChampionUrl';
@@ -19,11 +21,13 @@ class Header extends Component {
             <div className="row">
               <div className="col-12">
                 <div className="flex">
+                  <Link to="/">
                   <h1 className="h1">Battlerite
                     <svg className="center" viewBox="0 0 508.928 508.928">
                       <g> <polygon points="403.712,201.04 256.288,201.04 329.792,0 105.216,307.888 252.64,307.888 179.136,508.928" /> </g>
                     </svg>
                     Pro</h1>
+                    </Link>
                   <h2 className="h2">Get all <span className="turquoise">stats</span> you need to improve your game</h2>
                 </div>
               </div>
