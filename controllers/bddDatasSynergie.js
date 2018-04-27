a5f04d2dbd0c46d6895e85f11697302eb10e1fff
vincentpelage <vincent.plge@gmail.com>
2018-04-27 19:22:55 +1000

    refacto back

--- controllers/bddDatasSynergie.js
+++ controllers/bddDatasSynergie.js
@@ -29,7 +29,7 @@ db.once('open', () => {
           {
             $match: {
               $and: [
-                { 'stats.won': 'true' },
+                // { 'stats.won': 'false' },
                 { 'actorId': champion.id },
               ]
             }
@@ -37,9 +37,29 @@ db.once('open', () => {
           {
             $group: {
             _id: {
-              won: '$stats.won',
-              teamIds: '$teamIds',
-              gameType: '$match.gameType',
+              // won: '$stats.won',
+              'teamIds': '$teamIds',
+              'gameType': '$match.gameType',
+            },
+            'won': {
+              '$sum': {
+                '$cond': [{
+                  '$eq': [
+                    '$stats.won',
+                    'true'
+                  ]
+                }, 1, 0]
+              }
+            },
+            'lost': {
+              '$sum': {
+                '$cond': [{
+                  '$eq': [
+                    '$stats.won',
+                    'false'
+                  ]
+                }, 1, 0]
+              }
             },
             count: { $sum: 1 } }
           }
@@ -60,10 +80,17 @@ db.once('open', () => {
                 return count;
               }
               comptage().then(function(count){
+                result['countTotal'] = count;
+
+                // if(result._id.gameType == 'LEAGUE2V2'){
+                //   result['winrate'] = Number(((result.count / (count / 2)) * 100).toFixed(1));
+                // } else {
+                //   result['winrate'] = Number(((result.count / (count / 3)) * 100).toFixed(1));
+                // }
                 if(result._id.gameType == 'LEAGUE2V2'){
-                  result['winrate'] = Number(((result.count / (count / 2)) * 100).toFixed(1));
+                  result['winrate'] = Number(((result.won) / (count / 2) * 100).toFixed(1));
                 } else {
-                  result['winrate'] = Number(((result.count / (count / 3)) * 100).toFixed(1));
+                  result['winrate'] = Number(((result.won) / (count / 3) * 100).toFixed(1));
                 }
 
                 arrRes.push(result);
@@ -85,25 +112,25 @@ db.once('open', () => {
                     }
                   })
 
-                  const dataTrio = arrRes.filter(obj => {
-                    return obj._id.gameType == 'LEAGUE3V3' && obj.count > 15;
-                  })
-                  data[champion.id]['trio'] = dataTrio.slice(0, 10);
-                  data[champion.id]['trio'].map(elem => {
-                    if(Number(elem._id.teamIds[0]) !== Number(champion.id)){
-                      if(Number(elem._id.teamIds[1]) !== Number(champion.id)){
-                        const mate1 = elem._id.teamIds[0];
-                        const mate2 = elem._id.teamIds[1];
-                        elem._id.teamIds = elem._id.teamIds.slice(2);
-                        elem._id.teamIds.push(mate1);
-                        elem._id.teamIds.push(mate2);
-                      } else {
-                        const mate = elem._id.teamIds[0];
-                        elem._id.teamIds.shift();
-                        elem._id.teamIds.push(mate)
-                      }
-                    }
-                  })
+                  // const dataTrio = arrRes.filter(obj => {
+                  //   return obj._id.gameType == 'LEAGUE3V3' && obj.count > 15;
+                  // })
+                  // data[champion.id]['trio'] = dataTrio.slice(0, 10);
+                  // data[champion.id]['trio'].map(elem => {
+                  //   if(Number(elem._id.teamIds[0]) !== Number(champion.id)){
+                  //     if(Number(elem._id.teamIds[1]) !== Number(champion.id)){
+                  //       const mate1 = elem._id.teamIds[0];
+                  //       const mate2 = elem._id.teamIds[1];
+                  //       elem._id.teamIds = elem._id.teamIds.slice(2);
+                  //       elem._id.teamIds.push(mate1);
+                  //       elem._id.teamIds.push(mate2);
+                  //     } else {
+                  //       const mate = elem._id.teamIds[0];
+                  //       elem._id.teamIds.shift();
+                  //       elem._id.teamIds.push(mate)
+                  //     }
+                  //   }
+                  // })
 
                   db.collection('datafronts').update(
                     { id: 1 },
